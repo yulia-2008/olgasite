df15d458b7d588a83ce452a1d940742ae8105b61
yulia <juliana.ny2008@gmail.com>
2023-12-15 12:59:22 -0500

    fix

--- src/Photo.js
+++ src/Photo.js
@@ -5,7 +5,7 @@ import GalleryMobile from './GalleryMobile1.jpg';
 class Photo extends Component {
     render() {
         return (
-            <div id='component' style={{opacity: 1}}>          
+            <div id='component-gallery'>          
                <img id='five-pics' src={window.innerWidth < 600 ? 
                    GalleryMobile : OlgaPics}></img>  
             </div>
