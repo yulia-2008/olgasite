3d042611fa8d82c5c6c99d09f8406f1341328378
yulia <juliana.ny2008@gmail.com>
2023-12-15 12:41:20 -0500

    fix

--- src/About.js
+++ src/About.js
@@ -7,20 +7,15 @@ class About extends Component {
     
 
     render() {  
-        return (
-       
-            <div id='component' style={window.innerWidth > 600 ? 
-                {width:'45%'}: {}
-            }>
-                
+        return (    
+            <div id='component' 
+                 style={window.innerWidth > 600 ? 
+                   {width:'45%'}: {}
+                }>    
                 <p style={{lineHeight: '14pt'}}>About Olga...Olga is a New York city base acress.
                     She started acting since she moved to the USA. At first it was
                     backgroung acting on TV and Olga was inspired of this magical world. </p>     
-            </div>  
-            
-                           
-                
-           
+            </div>     
         );
     }
 }
--- src/Contact.js
+++ src/Contact.js
@@ -4,9 +4,10 @@ import React, { Component } from 'react';
 class Contact extends Component {
     render() {
         return (
-            <div id='component' style={window.innerWidth > 600 ? 
-                {width:'20%'}: {}
-                }>            
+            <div id='component' 
+                 style={window.innerWidth > 600 ? 
+                    {width:'20%'}: {}
+                 }>            
                     <h4>Contact</h4> 
                     <p>olgasopian@gmail.com</p>                
                 </div>          
